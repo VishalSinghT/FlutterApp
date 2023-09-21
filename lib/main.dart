f9f0fc05b54df59df65f114c16e943bec61d4af4
VishalSinghT <vishalthakurt53@gmail.com>
2023-09-21 13:13:21 +0530

    flApp

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:learning/Screens/home_Scrren.dart';
+
+import 'Screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+      
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      routes: {
+        'home': (context)=>WelcomeScreen(),
+      },
+      initialRoute: 'home',
+      
+      
+    );
+  }
+}
+
